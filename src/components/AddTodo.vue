9141cd37665687d58cda17261186411fe61d30f7
Elihle <lihlemenz@gmail.com>
2020-03-10 10:54:49 +0200

    initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit="addTodo">
+        <input type="text" v-model="title" name="title" placeholder="Add Todo . . .">
+        <input type="submit" value="Submit" class="btn">
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo',
+    data() {
+        return {
+            title: ''
+        }
+    }
+}
+</script>
+
+<style scoped>
+    form {
+        display: flex;
+    }
+
+    input[type="text"] {
+        flex: 10;
+        padding: 5px;
+    }
+
+        input[type="sub,it"] {
+        flex: 2;
+    }
+</style>
